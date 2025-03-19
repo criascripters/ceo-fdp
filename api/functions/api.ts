07a1d73894ee447e495b237572f1b16d55a5d6e9
Alex Oliveira <contatoalexandergv@gmail.com>
2025-03-19 01:05:09 -0300

    feat:implementing prisma

--- api/functions/api.ts
+++ api/functions/api.ts
@@ -1,13 +1,36 @@
 import express, { Request, Response } from "express";
 import serverless from "serverless-http";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
 
 const app = express();
 app.use(express.json());
 const router = express.Router();
 
+async function main() {
+  await prisma.$connect();
+}
+
+main().catch(console.error);
+
 router.get("/", (req, res) => {
   res.json({
     message: "hello world",
+  });
+});
+
+router.get("/test", async (req: Request, res: Response) => {
+  await prisma.message.create({
+    data: {
+      message: "hello world",
+      name: "aaa",
+    },
+  });
+
+  res.json({
+    message: "created",
+    status: 200,
   });
 });
 
