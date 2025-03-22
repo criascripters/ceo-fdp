3dc2a3c0fe14a4c66a358c5a68a94b9e9c898ab5
Alexandre Batistella Bellas <alexandre.bellas@gmail.com>
2025-03-21 22:36:35 -0300

    feat: make api listen to port 3000 when in dev env

--- api/src/functions/api.ts
+++ api/src/functions/api.ts
@@ -1,11 +1,17 @@
+import express from "express";
 import serverless from "serverless-http";
-import express, { Request, Response } from "express";
-import router from "../routes";
 import "../database";
+import router from "../routes";
 
 const app = express();
 app.use(express.json());
 app.use("/", router);
+
+if (process.env.NODE_ENV === "dev") {
+    app.listen(3000, () => {
+        console.log("Server is running on port 3000");
+    });
+}
 
 export default app;
 
