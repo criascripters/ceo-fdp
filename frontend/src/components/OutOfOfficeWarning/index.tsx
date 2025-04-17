4d4f75d8cf8d9e1656de8d7a24644b7ceb124218
Alex Oliveira <contatoalexandergv@gmail.com>
2025-04-17 15:55:20 -0300

    fix: fixed prod merge

--- frontend/src/components/OutOfOfficeWarning/index.tsx
+++ frontend/src/components/OutOfOfficeWarning/index.tsx
@@ -5,22 +5,24 @@ import "./index.css";
  * Alerta que aparece quando o P.O. está fora do seu horário de cobrança.
  */
 export default function OutOfOfficeWarning() {
-    // #region States
-    const showWarning = useMemo(() => {
-        const currentHour = new Date().getHours();
-        return currentHour >= 18 || currentHour < 8
-    }, []);
-    // #endregion
+  // #region States
+  const showWarning = useMemo(() => {
+    const currentHour = new Date().getHours();
+    return currentHour <= 18 || currentHour < 8;
+  }, []);
+  // #endregion
 
-    if (!showWarning) return <></>
+  if (!showWarning) return <></>;
 
-    return (
-        <div className="flex items-center justify-center gap-2 flex-none w-full p-2 leading-none
+  return (
+    <div
+      className="flex items-center justify-center gap-2 flex-none w-full p-2 leading-none
             text-center font-bold text-[0.85rem]
             bg-red-500 text-white
-        ">
-            <img className="clock" src="./clock.png" alt="relógio" />
-            <span>JÁ PASSOU DAS 18H MALUCO, TO NO BAR BEBENDO. VOLTO A MANDAR MENSAGENS AMANHÃ ÀS 8H</span>
-        </div>
-    )
+        "
+    >
+      <img className="clock" src="./clock.png" alt="relógio" />
+      <span>JÁ PASSOU DAS 18H MALUCO, TO NO BAR BEBENDO. VOLTO A MANDAR MENSAGENS AMANHÃ ÀS 8H</span>
+    </div>
+  );
 }
