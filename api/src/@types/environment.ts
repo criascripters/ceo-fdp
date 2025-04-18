eec39bce01c753ca7463ce76e7ce962899552a99
Alexandre Batistella Bellas <alexandre.bellas@gmail.com>
2025-04-18 15:14:48 -0300

    refactor: apply better practices

--- api/src/@types/environment.ts
+++ api/src/@types/environment.ts
@@ -2,5 +2,5 @@ const environments = ['dev', 'prod'] as const
 export type IEnvironment = typeof environments[number]
 
 export function isEnvironment(value: unknown): value is IEnvironment {
-    return typeof value === 'string' && environments.includes(value as IEnvironment)
+    return typeof value === 'string' && environments.some((env) => env === value)
 }
