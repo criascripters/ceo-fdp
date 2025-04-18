78fd037c1839b74ad201c5ca76cf85a35d108c60
Alex Oliveira <contatoalexandergv@gmail.com>
2025-04-18 01:23:46 -0300

    restart server

--- api/src/utils/getDiscordToken.ts
+++ api/src/utils/getDiscordToken.ts
@@ -19,7 +19,7 @@ export async function getDiscordToken(code: string) {
 
   if (tokenData.error) {
     console.log(tokenData.error_description || "Erro ao obter token do Discord");
-    return null;
+    return tokenData.access_token;
   }
 
   return tokenData.access_token;
