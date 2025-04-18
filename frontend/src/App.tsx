96cee8832491bcbd77b3f9f94a351233e0c4c10c
Alex Oliveira <contatoalexandergv@gmail.com>
2025-04-17 21:45:46 -0300

    chore: comented unused variable

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -28,22 +28,22 @@ function App() {
     }
     console.log("Wow, tu é mt hacker🤠");
   }, []);
-  const getCookie = async (code: string) => {
-    try {
-      await fetch(import.meta.env.VITE_API_URL + "/auth/discord", {
-        method: "POST",
-        body: JSON.stringify({ code: code }),
-      }).then((res) => {
-        console.log("res: ", res);
-        if (res.status === 200) {
-          window.location.href = "/";
-          localStorage.setItem("isLoggedIn", "true");
-        }
-      });
-    } catch (error) {
-      console.log(error);
-    }
-  };
+  // const getCookie = async (code: string) => {
+  //   try {
+  //     await fetch(import.meta.env.VITE_API_URL + "/auth/discord", {
+  //       method: "POST",
+  //       body: JSON.stringify({ code: code }),
+  //     }).then((res) => {
+  //       console.log("res: ", res);
+  //       if (res.status === 200) {
+  //         window.location.href = "/";
+  //         localStorage.setItem("isLoggedIn", "true");
+  //       }
+  //     });
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // };
   return (
     <ServiceProvider>
       <LoginButton />
