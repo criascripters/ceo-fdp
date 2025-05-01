88ddddc9cb967314414134a816c1942ec706ec05
Alexandre Batistella Bellas <alexandre.bellas@gmail.com>
2025-05-01 18:33:43 -0300

    feat: improve API router structure and add auth middleware

--- api/src/middleware/apiAuth.ts
+++ api/src/middleware/apiAuth.ts
@@ -1,12 +1,58 @@
 import { NextFunction, Request, Response } from 'express'
+import IsAdminService from '../services/users/is-admin'
+import { getDiscordUserInfo } from '../utils/getDiscordUserInfo'
 import { settings } from '../utils/settings'
 
-export function apiAuth(req: Request, res: Response, next: NextFunction) {
-  const auth = req.headers.authorization
+/**
+ * Middleware para autenticar administradores.
+ */
+export function isAdmin(req: Request, res: Response, next: NextFunction) {
+  // Try cookie
+  const cookieToken = req.cookies?.token
 
-  if (!auth || auth !== `Bearer ${settings.apiSecret}`) {
-    return res.status(403).json({ error: 'Forbidden' })
+  if (!cookieToken || typeof cookieToken !== 'string') {
+    // Try Bearer token
+    const auth = req.headers.authorization
+
+    if (!auth || auth !== `Bearer ${settings.apiSecret}`) {
+      res.sendStatus(401)
+      return
+    }
   }
 
-  next()
+  const isAdminService = new IsAdminService()
+  isAdminService.execute(cookieToken).then((isAdmin) => {
+    if (!isAdmin) {
+      res.sendStatus(401)
+      return
+    }
+
+    next()
+  })
+}
+
+/**
+ * Middleware para autenticar usuários comuns.
+ */
+export function isAuthenticated(req: Request, res: Response, next: NextFunction) {
+  const cookieToken = req.cookies?.token
+
+  if (!cookieToken || typeof cookieToken !== 'string') {
+    res.sendStatus(401)
+    return
+  }
+
+  getDiscordUserInfo(cookieToken)
+    .then((discordUser) => {
+      if (!discordUser) {
+        res.sendStatus(401)
+        return
+      }
+
+      next()
+    })
+    .catch((err) => {
+      console.log(err)
+      res.sendStatus(401)
+    })
 }
--- api/src/utils/getDiscordUserInfo.ts
+++ api/src/utils/getDiscordUserInfo.ts
@@ -1,7 +1,6 @@
-import { APIUser } from 'discord-api-types/v10'
 import oauth from './oauth'
 
-export async function getDiscordUserInfo(accessToken: string): Promise<APIUser> {
+export async function getDiscordUserInfo(accessToken: string) {
   const res = await oauth.getUser(accessToken)
 
   return res
